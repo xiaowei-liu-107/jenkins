151b54fc45a189f51fc03278403cf8e109c2e821
qaq <qaq@qaqdeMacBook-Air.local>
2019-11-27 05:46:13 -0800

    first commit

---
+++ scripts/test01.py
@@ -0,0 +1,16 @@
+class TestLogin:
+
+    def test_login_001(self):
+
+        print("ff")
+        assert 0
+
+    def test_login_002(self):
+
+        print("ss")
+        assert 0
+
+    def test_login_003(self):
+
+        print("ss")
+        assert 1
